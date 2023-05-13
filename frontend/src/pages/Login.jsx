b77a676ede5e7cea2d488466ee01ee180b9f5e81
Kunyelewosi Alexander <kunalexander360@gmail.com>
2023-05-13 01:37:45 +0000

    login and register forms set up

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,58 @@
+import { useState , useEffect} from "react";
+import { FaSignInAlt } from "react-icons/fa";
 
 function Login() {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        password2: ""
+    });
+    const {email, password} = formData;
+    const onChange = (e) => {
+        setFormData(prevState => ({...prevState, [e.target.name]: e.target.value}))
+    }
+    const onSubmit = () => {
+
+    }
   return (
-    <div>Login</div>
+    <>
+        <section className="heading">
+            <h1>
+                <FaSignInAlt/>
+            </h1>
+            <p>Please log in</p>
+        </section>
+        <section className="form">
+            <form onSubmit= {onSubmit}>
+               
+
+                <div className="form-group">
+                <input type="email" 
+                className= "form-control" 
+                id="email" name="email" 
+                value={email}
+                onChange={onChange} 
+                placeholder="Enter your email"/>
+                </div>
+
+                <div className="form-group">
+                <input type="password" 
+                className= "form-control" 
+                id="password" name="password" 
+                value={password}
+                onChange={onChange} 
+                placeholder="Enter password"/>
+                </div>
+
+
+
+                <div className="form-group">
+                    <button type="submit" className="btn btn-block">Submit</button>
+                </div>
+            </form>
+        </section>
+    </>
   )
 }
 
